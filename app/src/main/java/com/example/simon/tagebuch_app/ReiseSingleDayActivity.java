243062332ce247e8a97dca18e1d0e002bcd80516
maxschoe <asperitias@googlemail.com>
2017-09-06 12:56:46 +0200

    cmas

---
+++ app/src/main/java/com/example/simon/tagebuch_app/ReiseSingleDayActivity.java
@@ -0,0 +1,13 @@
+package com.example.simon.tagebuch_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReiseSingleDayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reise_single_day);
+    }
+}
