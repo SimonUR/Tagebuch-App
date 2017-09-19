c982a96eb40e2b89e17c5cf364b357204ee33da8
taniCode <artanndreka@hotmail.com>
2017-09-19 21:44:29 +0200

    new

--- app/src/main/java/com/example/simon/tagebuch_app/RegistryActivity.java
+++ app/src/main/java/com/example/simon/tagebuch_app/RegistryActivity.java
@@ -1,32 +1,32 @@
 package com.example.simon.tagebuch_app;
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.app.AlertDialog.Builder;
+import android.widget.Toast;
+
+import com.example.simon.tagebuch_app.databases.RegistryDB;
 
 public class RegistryActivity extends AppCompatActivity {
 
     private EditText inputName, inputEmail, inputPasswort, inputPasswortWieder;
     private Button abschicken;
-    SQLiteDatabase db;
+    private RegistryDB myDb;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registry);
 
+        myDb = new RegistryDB(this);
         inputName = (EditText) findViewById(R.id.name);
         inputEmail = (EditText) findViewById(R.id.email);
         inputPasswort = (EditText) findViewById(R.id.passwort);
         inputPasswortWieder = (EditText) findViewById(R.id.password_bestätigen);
-
-        db = openOrCreateDatabase("Benutzer", Context.MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS benutzer(name VARCHAR,email VARCHAR,passwort VARCHAR, passwort VARCHAR);");
 
 
         abschicken = (Button) findViewById(R.id.abschicken);
@@ -34,24 +34,25 @@ public class RegistryActivity extends AppCompatActivity {
         abschicken.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (v == abschicken) {
-                    //checking for blank fields
-                    if (inputName.getText().toString().trim().length() == 0 ||
-                            inputEmail.getText().toString().trim().length() == 0 ||
-                            inputPasswort.getText().toString().trim().length() == 0 ||
-                            inputPasswortWieder.getText().toString().trim().length() == 0) {
-                        alert("Error", "Please enter all fields");
-                        return;
-                    }
-                    // inserting values to DB.
-                    db.execSQL("INSERT INTO benutzer VALUES('" + inputName.getText() + "','" + inputEmail.getText() +
-                            "','" + inputPasswort.getText() + "','" + inputPasswortWieder.getText() + "'");
-                    alert("Success", "Record added");
-                    clearText();
-                }
-
+                readDaten();
             }
+
         });
+
+    }
+    private void readDaten() {
+        String name = inputName.getText().toString();
+        String email = inputEmail.getText().toString();
+        String passwort = inputPasswort.getText().toString();
+        String passwortWieder = inputPasswortWieder.getText().toString();
+        Boolean result = myDb.insertData(name,email,passwort);
+        if (result == true && passwort == passwortWieder )
+        {
+            Toast.makeText(this, "Data Inserted Successfully", Toast.LENGTH_SHORT).show();
+            clearText();
+        }else {
+            Toast.makeText(this, "Data Inserted Failed!",Toast.LENGTH_SHORT).show();
+        }
     }
 
 
@@ -72,11 +73,4 @@ public class RegistryActivity extends AppCompatActivity {
     }
     }
 
-  /*  private void readDaten() {
-        String name = inputName.getText().toString();
-        String email = inputEmail.getText().toString();
-        String passwort = inputPasswort.getText().toString();
-        String passwortWieder = inputPasswortWieder.getText().toString();
-
-    } */
 
---
+++ app/src/main/java/com/example/simon/tagebuch_app/databases/RegistryDB.java
@@ -0,0 +1,57 @@
+package com.example.simon.tagebuch_app.databases;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.ContentValues;
+
+/**
+ * Created by tani on 19.09.2017.
+ */
+
+
+public class RegistryDB extends SQLiteOpenHelper{
+    public static final String DATABASE_NAME = "RegistryDB";
+    public static final String TABLE_NAME = "benutzer";
+
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "EMAIL";
+    public static final String COL_4 = "PASSWORT";
+
+    public RegistryDB(Context context)
+    {
+        super(context,DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        db.execSQL("CREATE TABLE" + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT,EMAIL TEXT, PASSWORT TEXT)");
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        db.execSQL("DROP TABLE IF EXIST" + TABLE_NAME);
+    }
+
+    public boolean insertData(String name, String email, String passwort)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, email);
+        contentValues.put(COL_4, passwort);
+        long result = db.insert(TABLE_NAME,null,contentValues);
+        db.close();
+
+        if (result== -1)
+        {
+            return false;
+        }else {
+            return true;
+        }
+
+    }
+
+}
