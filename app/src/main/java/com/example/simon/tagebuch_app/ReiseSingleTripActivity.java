dc36276acee3857fdbde61d501f819bb55841da8
SimonUR <simon-skater@gmx.de>
2017-09-20 15:01:26 +0200

    2. liste

--- app/src/main/java/com/example/simon/tagebuch_app/ReiseSingleTripActivity.java
+++ app/src/main/java/com/example/simon/tagebuch_app/ReiseSingleTripActivity.java
@@ -1,16 +1,23 @@
 package com.example.simon.tagebuch_app;
 
 import android.content.Intent;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ListView;
 
 import com.example.simon.tagebuch_app.databases.reiseTageDatabase;
 import com.example.simon.tagebuch_app.reise.Reisetag;
 import com.example.simon.tagebuch_app.reise.adapterFuerReiseTage;
 
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 public class ReiseSingleTripActivity extends AppCompatActivity {
 
@@ -36,18 +43,41 @@ public class ReiseSingleTripActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
+
     private void addDaysToList() {
         Intent intent = getIntent();
         Bundle tripInfo = intent.getExtras();
         String tripName = tripInfo.get("Reisename").toString();
         String startDate = tripInfo.get("Startdatum").toString();
         String endDate = tripInfo.get("Enddatum").toString();
-        Log.v("NAEM", tripName);
-        Log.v("DATE", startDate);
 
+        Date formattedStartDate = null;
+        Date formattedEndDate = null;
+
+
+        try{
+            formattedStartDate = new SimpleDateFormat("dd.MM.yyyy").parse(startDate);
+            formattedEndDate = new SimpleDateFormat("dd.MM.yyyy").parse(endDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        Calendar cal2 = Calendar.getInstance(Locale.GERMAN);
+        Calendar cal1 = Calendar.getInstance(Locale.GERMAN);
+
+        cal1.setTime(formattedEndDate);
+        cal2.setTime(formattedStartDate);
+
+        System.out.println("Days= " + daysBetween(cal2.getTime(), cal1.getTime()));
+
+        startDate = cal2.get(Calendar.DAY_OF_MONTH) + "." + cal2.get(Calendar.MONTH) + "." + cal2.get(Calendar.YEAR);
         if (!tripName.equals("") && !startDate.equals("") && !endDate.equals("")) {
             addNewTrip(tripName, startDate, endDate);
         }
+    }
+
+    public int daysBetween(Date d1, Date d2){
+        return (int)( (d2.getTime() - d1.getTime()) / (1000 * 60 * 60 * 24));
     }
 
     private void addNewTrip(String tripName, String startDate, String endDate) {
